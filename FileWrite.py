1a7d604cd3cc5d855c0dc24b1c107c23d7abb745
MaxGuidry <mguidry7@outlook.com>
2017-02-24 17:23:56 -0600

    more testing and syntax

--- FileWrite.py
+++ FileWrite.py
@@ -1,2 +1,8 @@
-nigs = open('testing.txt','w')
-nigs.write("nigs nigs nigs")
+
+
+def main():
+    aNewFile = open('testing.txt','w')
+    aNewFile.write("I wrote this here ")    # display some lines
+
+
+if __name__ == "__main__": main()
