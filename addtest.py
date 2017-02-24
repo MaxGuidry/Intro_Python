bd445bc27f6736d68d072e95d460db74727a5842
MaxGuidry <mguidry7@outlook.com>
2017-02-24 12:51:20 -0600

    initial commit

---
+++ addtest.py
@@ -0,0 +1,10 @@
+class nigger:
+	m_name ='just a nig'
+	m_age=30
+	m_color='nigger color'
+	
+	def __init__(self,name,age,color):
+		self.m_age=age
+		self.m_name=name
+		self.m_color=color
+	
---
+++ test.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import os
+import addtest
+nigs = ['donray','max','regi',2,3,4]
+print'first nig: ',nigs[0]
+
+for x in range(0,nigs.__len__()):
+	print nigs[x]
+
+Donray = addtest.nigger("don",40,"black")
+Regi = addtest.nigger
+
+print Regi.m_name
+print Donray.m_name
+
+raw_input()
+
