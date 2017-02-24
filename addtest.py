116e3ce5bf26b67cd836e816e7ee94cdab5e3571
MaxGuidry <mguidry7@outlook.com>
2017-02-24 14:47:43 -0600

    Learning the syntax of python

--- addtest.py
+++ addtest.py
@@ -1,10 +1,22 @@
-class nigger:
-	m_name ='just a nig'
-	m_age=30
-	m_color='nigger color'
+import math
+
+class Vector3:
+	x=0
+	y=0
+	z=0
 	
-	def __init__(self,name,age,color):
-		self.m_age=age
-		self.m_name=name
-		self.m_color=color
+	def __init__(self,ex,why,zee):
+		self.x=ex
+		self.y=why
+		self.z=zee
 	
+	def Magnitude(self):
+		return math.sqrt(self.x**2+self.y**2+self.z**2)
+		
+	def Normalized(self):
+		return Vector3(self.x/self.Magnitude(),self.y/self.Magnitude(),self.z/self.Magnitude())
+		
+	def Print(self):
+		print self.x
+		print self.y
+		print self.z
